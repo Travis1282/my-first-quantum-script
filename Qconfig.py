a6cf23ea701d9722b0229e6e8593d0e2b0086aa5
Travis1282 <travisjosephclark@gmail.com>
2018-06-04 21:29:47 -0500

    initial commit, api is being called

---
+++ Qconfig.py
@@ -0,0 +1,5 @@
+APItoken ='61030f7d4b49db24cc1963b52b853d9b9ab70ad73eb75054dd0f6079e75a4c19f614b85ea427f8f16799b3a11005023c1587c26ad04d9f49a8c2160c7dd74944'
+
+config = {
+    'url': 'https://quantumexperience.ng.bluemix.net/api'
+}
